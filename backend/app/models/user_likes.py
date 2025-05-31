93eba2d3e3e2f0aebfa73be2e0983a8ec45eb26d
viny.tito <viny.palazuelos@gmail.com>
2025-05-30 21:23:23 -0700

    x

--- backend/app/models/user_likes.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from app.database import Base
-
-user_likes = Table(
-    "user_likes",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
-    Column("book_id", Integer, ForeignKey("books.id"), primary_key=True)
-)
